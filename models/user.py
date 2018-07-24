26eb650d3846ff064079dbaaa0589ef6f2e8ca77
Andrew Keil <monkeyninja147@gmail.com>
2018-07-24 23:20:31 +0000

    add ophan delete to user.py

--- models/user.py
+++ models/user.py
@@ -16,4 +16,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="all, delete")
+    places = relationship(
+        "Place", backref="user", cascade="all, delete-orphan")
