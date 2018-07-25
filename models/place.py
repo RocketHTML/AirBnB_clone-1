a513833165bd5b6c8c77bee36a8e7bab0d49009f
Andrew Keil <monkeyninja147@gmail.com>
2018-07-25 01:47:15 +0000

    Files for #9

--- models/place.py
+++ models/place.py
@@ -11,6 +11,21 @@ class Place(BaseModel, Base):
         Define the class Place that inherits from BaseModel.
     '''
     __tablename__ = 'places'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref="place", cascade="delete")
+    else:
+        @property
+        def reviews(self):
+            """
+            reviews getter
+            """
+            review_dict = models.storage.all(Review)
+            review_list = []
+            for review in review_dict.values():
+                if review.place_id == self.id
+                review_list.append(review)
+            return review
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60),  ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
