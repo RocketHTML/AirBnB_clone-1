fb2188833c2d8155bb15a0bab6da98ab4c88d541
Andrew Keil <monkeyninja147@gmail.com>
2018-07-25 16:52:20 +0000

    Updated files for #10 intial

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,20 @@
 '''
     Implementation of the Amenity class
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import place
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''
         Implementation for the Amenities.
     '''
-    name = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary="place_amenity")
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -3,9 +3,20 @@
     Define the class Place.
 '''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 import os
+
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     '''
@@ -14,12 +25,35 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship("Amenity",
+                                 secondary=place_amenity, viewonly=False)
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        amenity_ids = []
+
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
-            """
-            reviews getter
-            """
             review_dict = models.storage.all(Review)
             review_list = []
             for review in review_dict.values():
@@ -27,14 +61,17 @@ class Place(BaseModel, Base):
                     review_list.append(review)
             return review
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
+        @property
+        def amenities(self):
+            amenity_list = []
+            for amenity in amenity_ids:
+                key = 'Amenity.{}'.format(id)
+                if key in self.__objects.keys():
+                    amenity_list.append(self.__objects[key])
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
+            else:
+                pass
