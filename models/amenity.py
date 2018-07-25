5d31b0d9b0dc3ef876f88b37c807d6135bc39675
Andrew Keil <monkeyninja147@gmail.com>
2018-07-25 05:51:43 +0000

    reverting to earlier state place amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,21 +2,11 @@
 '''
     Implementation of the Amenity class
 '''
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models import place
-import os
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=place.place_amenity,
-                                       viewonly=False)
-    else:
-        name = ""
+    name = ""
