b1279497051f4be16c75ff443e107bb7ee5d8e08
Andrew Keil <monkeyninja147@gmail.com>
2018-07-25 03:57:37 +0000

    In-progress files for #10

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,17 @@
 '''
     Implementation of the Amenity class
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import place
 
 
 class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place.place_amenity,
+                                   viewonly=False)
--- models/place.py
+++ models/place.py
@@ -7,6 +7,15 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 import os
 
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False))
+
+
 class Place(BaseModel, Base):
     '''
         Define the class Place that inherits from BaseModel.
