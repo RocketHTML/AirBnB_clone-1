897327e0d9a34631b849035dfb1284f304c41cdf
Madison Burke <madisononealburke@gmail.com>
2018-07-25 18:47:32 -0700

    revert

--- models/amenity.py
+++ models/amenity.py
@@ -7,16 +7,15 @@ import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models import place
-import os
 
 
 class Amenity(BaseModel, Base):
     '''
         Implementation for the Amenities.
     '''
-    __tablename__ = 'amenities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary="place_amenity", backref="amenities")
+        place_amenities = relationship('Place', secondary="place_amenity")
     else:
         name = ""
