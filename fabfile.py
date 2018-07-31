55092906a3915dad6955ab5ffaf4afc9610db28d
rubyshadows <343@holbertonschool.com>
2018-07-30 22:37:50 -0700

    testing fabric

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Setup and Deploy on servers
+"""
+from fabric.api import *
+
+
+web1 = "35.237.187.19"
+web2 = "35.196.245.228"
+lb = "104.196.146.126 lb"
+
+
+
+def configure_nginx():
+    """ 
+        Push and execute either bash or puppet file.
+        They'll create /data/webstatic/[releases|shared] dirs.
+            As well as install nginx and point it at a default index.html
+    """
+    env.hosts = [web1, web2]
+    env.user = 'root'
+    env.key_filename = '/root/.ssh/id_rsa'
+
+    put('./0-setup_web_static.sh', '/tmp/server_setup.sh')
