1ecda0302470bf0d51232186e0b728804f90a824
root <madisononealburke@gmail.com>
2018-07-31 06:54:33 +0000

    add simpler html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,18 @@
 # Configured nginx server for custom header
 location="location /hbnb_static {\n alias /data/web_static/current; \n}\n"
 file=/etc/nginx/sites-available/default
+html="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
 
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-curl dockerhearts.online > /data/web_static/releases/test/index.html
+sudo echo "$html" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 cp ${file}.bak $file
