36e6b74e397e74f1aa568213d4d3f68a1c52834f
root <madisononealburke@gmail.com>
2018-07-31 05:46:50 +0000

    move env outside function

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,9 @@ web1 = "35.237.187.19"
 web2 = "35.196.245.228"
 lb = "104.196.146.126 lb"
 
+env.hosts = [web1, web2]
+env.user = 'ubuntu'
+env.key_filename = '/root/.ssh/id_rsa'
 
 
 def configure_nginx():
@@ -17,8 +20,4 @@ def configure_nginx():
         They'll create /data/webstatic/[releases|shared] dirs.
             As well as install nginx and point it at a default index.html
     """
-    env.hosts = [web1, web2]
-    env.user = 'root'
-    env.key_filename = '/root/.ssh/id_rsa'
-
     put('./0-setup_web_static.sh', '/tmp/server_setup.sh')
