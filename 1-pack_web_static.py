2c32c96b321cfb756391e1b7861d596387bfc1b0
rubyshadows <343@holbertonschool.com>
2018-08-01 07:18:59 -0700

    add fabric tar packing

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Deploy web_static on servers
+"""
+from fabric.api import *
+from datetime import datetime
+import os
+
+web1 = "35.237.187.19"
+web2 = "35.196.245.228"
+lb = "104.196.146.126 lb"
+
+env.hosts = [web1, web2]
+env.user = 'ubuntu'
+env.key_filename = '/root/.ssh/id_rsa'
+
+def do_pack():
+    web = 'web_static'
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    tar = 'versions/{0}_{1}.tgz'.format(web, time)
+    local('mkdir versions')
+    local('tar cvzf {} {}'.format(tar, web))
+    try:
+        size = os.path.getsize(tar)
+        print('{} packed: {} -> {}Bytes'.format(web, tar, size))
+        return tar
+    except os.error:
+        return None
