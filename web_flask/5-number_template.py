8e8bedda85671ea94f253e60be35b552c560292a
rubyshadows <343@holbertonschool.com>
2018-08-19 21:42:13 -0700

    html template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    This is a script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def landing_page():
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb():
+    return("HBNB")
+
+
+@app.route('/c/<text>')
+def c(text):
+    return("C {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def numtemplate(n):
+    return(render_template('5-number.html', n=n))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def pyroute(text='is cool'):
+    return("Python {}".format(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
